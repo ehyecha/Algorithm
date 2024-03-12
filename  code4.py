b76c41796ca63ca049e44c3765cadbae02cede14
ehyecha <hyeonyeong88@gmail.com>
2024-03-13 00:14:34 +0900

    bug fix code

---  code4.py
+++  code4.py
@@ -9,25 +9,22 @@ def solution(bandage, health, attacks):
     current_health = health
     idx = 0
     for time in range(0, attacks[-1][0] + 1):
+        successive_score += 1
         attack_time = [i[0] for i in attacks]
         attack_degree = [i[1] for i in attacks]
         
         if time in attack_time:
             current_health -= attack_degree[idx]
-            current_health = isMaxHeal(current_health, health)
             idx += 1
             successive_score = 0
         else:
             if current_health < health:
                 current_health += bandage[1]
-                current_health = isMaxHeal(current_health, health)
         
         if successive_score == bandage[0]:
-             current_health +=bandage[2]
-             if current_health > health:
-                current_health = health
-        
-        successive_score += 1
+             current_health += bandage[2]
+             current_health = isMaxHeal(current_health, health)
+                
         if current_health  <= 0:
             current_health = -1
             break;
