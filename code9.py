8156bf168025695d7a88157c0a6236127c0e70f8
ehyecha <hyeonyeong88@gmail.com>
2024-03-17 23:18:04 +0900

    and problem name and path

--- code9.py
+++ code9.py
@@ -1,3 +1,5 @@
+#출처 : https://school.programmers.co.kr/learn/courses/30/lessons/181913
+#문자열 여러번 뒤집기
 def solution(my_string, queries):
    answer  = ''
    my_string = list(my_string)
@@ -6,5 +8,4 @@ def solution(my_string, queries):
         a = int(a)
         b = int(b)
         #문자열 뒤집기 진행하기
-      
    return my_string
