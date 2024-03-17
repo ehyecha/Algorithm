e1e07ee0b0957c39c587196ab77cccfa96504def
ehyecha <hyeonyeong88@gmail.com>
2024-03-17 23:12:01 +0900

    coding test day 9

---
+++ code9.py
@@ -0,0 +1,10 @@
+def solution(my_string, queries):
+   answer  = ''
+   my_string = list(my_string)
+   for q in queries:
+        a,b = q
+        a = int(a)
+        b = int(b)
+        #문자열 뒤집기 진행하기
+      
+   return my_string
