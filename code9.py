e52b9ec8b0cf156eb4068b4038d4a6af51d4d5a3
ehyecha <hyeonyeong88@gmail.com>
2024-03-18 12:44:54 +0900

    finish code

--- code9.py
+++ code9.py
@@ -1,11 +1,14 @@
 #출처 : https://school.programmers.co.kr/learn/courses/30/lessons/181913
 #문자열 여러번 뒤집기
 def solution(my_string, queries):
-   answer  = ''
    my_string = list(my_string)
    for q in queries:
         a,b = q
         a = int(a)
         b = int(b)
         #문자열 뒤집기 진행하기
-   return my_string
+        my_string = my_string[:a] + list(reversed(my_string[a:b +1])) + my_string[b+1:]
+   return ''.join(my_string)
+
+d = solution("rermgorpsam",	[[2, 3], [0, 7], [5, 9], [6, 10]])
+print(d)
