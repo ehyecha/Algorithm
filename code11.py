eac1e99adde7bba021882c20bd3e9d2f0ad36408
ehyecha <hyeonyeong88@gmail.com>
2024-03-19 22:57:53 +0900

    coding test day 11

---
+++ code11.py
@@ -0,0 +1,14 @@
+#출처: https://school.programmers.co.kr/learn/courses/30/lessons/120924
+#문제: 코딩테스트 연습 > 코딩테스트 입문 > 다음에 올 숫자
+
+
+def solution(common):
+    answer = 0
+    #6등차인지 등비인지 구별
+    diff1 =common[1] - common[0]
+    diff2 = common[2] - common[1]
+    if (diff1 == diff2):
+        answer = common[len(common) -1] + diff1
+    else :
+        answer =  common[len(common) -1] * (diff2 / diff1)
+    return answer
