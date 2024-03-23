15fdb223c389b15190bfbe18ee0451009cfb5ff8
ehyecha <hyeonyeong88@gmail.com>
2024-03-23 23:24:37 +0900

    add solution path

--- code15.py
+++ code15.py
@@ -1,3 +1,6 @@
+#출처: https://school.programmers.co.kr/learn/courses/30/lessons/138476
+#문제: 코딩테스트 연습> 연습문제 > 귤 고르기
+
 from collections import Counter
 def solution(k, tangerine):
     cte = Counter(tangerine)
