10e7f4ec1296fe7c428b670d03c4815932ec7c88
ehyecha <hyeonyeong88@gmail.com>
2024-03-24 23:12:12 +0900

    coding test day 16

---
+++  code16.py
@@ -0,0 +1,8 @@
+#출처 https://school.programmers.co.kr/learn/courses/30/lessons/60057
+#문제 코딩테스트 연습 2020 KAKAO BLIND RECRUITMENT 문자열 압축
+
+
+def solution(s):
+    answer = 0
+    return answer
+solution("aabbaccc")
