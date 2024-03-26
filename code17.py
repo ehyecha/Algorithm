c92d5eb522584fc25099b8116905c7765e52b78e
ehyecha <hyeonyeong88@gmail.com>
2024-03-26 23:45:22 +0900

    coding test day 17

---
+++ code17.py
@@ -0,0 +1,29 @@
+def solution(order):
+    answer, sub = initcontainer(order)
+    for i in order[answer:]:
+        if i == sub[-1]:
+            answer += 1
+            sub.pop()
+        else:
+            break
+    return answer       
+
+def initcontainer(order):
+    container = [ i +1 for i in range(len(order))]
+    sub_container = []
+    result = 0
+
+    for o in order:
+        if len(sub_container) > 0 and o == sub_container[-1]:
+            result += 1
+            sub_container.pop()
+            break;
+        for j in container:
+            if o == j :
+                result += 1
+                break
+            else:
+                sub_container.append(j)
+    return result, sub_container
+
+print(solution([4,2,1,3]))
