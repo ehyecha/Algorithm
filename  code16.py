1e5d472e4314bab95045be9a8918da6e63661fce
ehyecha <hyeonyeong88@gmail.com>
2024-03-26 09:42:03 +0900

    coding test day 16

---  code16.py
+++  code16.py
@@ -3,6 +3,31 @@
 
 
 def solution(s):
-    answer = 0
-    return answer
-solution("aabbaccc")
+    max_cnt = int(len(s) / 2)
+    target = []
+    if len(s) ==1:
+        return s
+    for i in range(1, max_cnt +1):
+        answer = ''
+        recursive = 0
+        pre = ''
+        for j in range(0, len(s), i):
+            cur = s[j:j+i]
+            if (pre != cur):
+                recursive = 0
+                answer += cur
+                pre = cur
+            else:
+                recursive += 1
+                answer = answer[:len(answer) - i]
+                if recursive > 1:
+                    answer = answer[: -len(str(recursive))]
+                answer += "{}{}".format(recursive + 1, cur)
+        target.append(len(answer))       
+    return min(target)
+
+a = solution("a")
+print(a)
+
+# s = 'dsafdsa'
+# print(s[-1])
