b454b760f6e99860a6de01a54ec661f6dcc12393
ehyecha <hyeonyeong88@gmail.com>
2024-03-28 12:37:58 +0900

    coding test day 19

--- code18.py
+++ code18.py
@@ -1,11 +1,6 @@
 #출처 : https://www.acmicpc.net/submit/31561/75781890
-#문제 : 백준 > 시계탑
+#문제 : 백준 2024 상반기 전남대학교 PIMM 알고리즘 파티 > 시계탑
 
-n = int(input())
-answer = 0
-if n >= 30:
-    n -= 30
-    answer = 15 + n *(3/2)
-else:
-    answer= n / 2
-print(answer)
+def solution():
+    n = int(input())
+    return  15 + (n -30) * 1.5 if  n >= 30 else n/2
---
+++ code19.py
@@ -0,0 +1,27 @@
+#출처 : https://www.acmicpc.net/problem/31562
+#문제 : 백준 2024 상반기 전남대학교 PIMM 알고리즘 파티 > 전주 듣고 노래 맞히기
+
+
+N, M = map(int, input().split())
+song = {}
+
+for _ in range(N):
+    T, S, a1, a2, a3, a4, a5, a6, a7 = input().split()
+    A = [a1, a2, a3]	# 첫 세 음만 리스트로 저장
+    song[S] = A		# key가 노래 제목, value가 첫 세 음
+
+for _ in range(M):
+    B = input().split()
+    count = 0	# 같은 노래가 몇 개인지 저장할 변수
+    title = ""	# 노래 제목 저장할 변수
+
+    for s in song:
+        if B == song[s]:	# 첫 세 음이 같다면
+            count += 1
+            title = s	# title 변수는 count가 1일때만 기능을 발휘한다.
+    if count >= 2:
+        print("?")
+    elif count == 1:
+        print(title)
+    else:
+        print("!")
