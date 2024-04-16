995a8a4e3e7b710fe98ca21e1949f497a062c679
ehyecha <hyeonyeong88@gmail.com>
2024-04-16 23:32:54 +0900

    coding test day 5

--- SQL/code5.sql
+++ SQL/code5.sql
@@ -1,3 +1,7 @@
 # 출처: https://school.programmers.co.kr/learn/courses/30/lessons/276034
 # 문제: 코딩테스트 연습 > SELECT >조건에 맞는 개발자 찾기
 
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME FROM DEVELOPERS
+WHERE SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python')
+    OR SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#')
+ORDER BY ID;
