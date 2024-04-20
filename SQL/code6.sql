8744f20302688cd7bff32e18a7398185b993089b
ehyecha <hyeonyeong88@gmail.com>
2024-04-20 23:56:15 +0900

    coding test day 6

---
+++ SQL/code6.sql
@@ -0,0 +1,9 @@
+#출처: https://school.programmers.co.kr/learn/courses/30/lessons/284531
+#문제: 코딩테스트 연습 > GROUP BY >노선별 평균 역 사이 거리 조회하기
+
+SELECT ROUTE, 
+CONCAT(ROUND(SUM(D_BETWEEN_DIST), 1),'km') AS TOTAL_DISTANCE, 
+CONCAT(ROUND(AVG(D_BETWEEN_DIST), 2), 'km') AS AVERAGE_DISTANCE
+FROM SUBWAY_DISTANCE
+GROUP BY ROUTE
+ORDER BY ROUND(SUM(D_BETWEEN_DIST), 1) DESC;
